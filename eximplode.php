5b244253ba4196c589ef5e31f8fb44db237ddcb5
Ubaidillah Aminuddin Thoyieb <43976788+ateritori@users.noreply.github.com>
2019-03-25 12:55:25 +0700

    Update eximplode.php

--- eximplode.php
+++ eximplode.php
@@ -1,5 +1,41 @@
 <?php
-$kalimat1 = "Apel, Blueberry, Cabe, Duku, Emping";
-$arr_kalimat1 = explode (", ",$kalimat1);
-var_dump(arr_kalimat1);
+//contoh Explode 1
+$data = "Ubaidillah, Gunungkidul, 28/07/1987, L, Islam";
+$arr_data = explode (", ",$data);
+$nama = $arr_data[0];
+echo "Dari Data : ".$data;
+echo "<br>";
+echo "Nama :".$nama;
+
+//contoh Explode 2
+$nik = "340 301 280787 0001";
+$arr_nik = explode (" ",$data);
+$tgl_lhr = $arr_nik[2];
+echo "Dari NIK : ".$nik;
+echo "<br>";
+echo "Tanggal Lahir :".$tgl_lahir;
+
+//contoh Explode 3
+$telpon = "0274-391019";
+$arr_telpon = explode ("-",$telpon);
+$kd_area = $arr_nik[0];
+echo "Dari Nomor Telpon : ".$telpon;
+echo "<br>";
+echo "Kode Area :".$kd_area;
+
+//contoh Explode 4
+$kd_terbang = "GA/16874";
+$arr_kd_terbang = explode ("/",$kd_terbang);
+$nmr_terbang = $arr_nik[1];
+echo "Dari Kode Penerbangan : ".$kd_terbang;
+echo "<br>";
+echo "Nomor Penerbangan :".$nmr_terbang;
+
+//contoh Explode 5
+$nosk = "09|Pem|III|2019";
+$arr_nosk = explode ("|",$nosk);
+$bln_sk = $arr_nosk[2];
+echo "DariNomor SK : ".$nosk;
+echo "<br>";
+echo "Bulan Terbit SK :".$nmr_terbang;
 ?>
